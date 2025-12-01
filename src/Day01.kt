92e8084abfa4bc3066a41cc4d5ebe1b3e47bc1d9
schmirgel <MCSIM.SCH@GMAIL.COM>
2025-12-01 14:55:31 +0100

    implement solution for day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -2,16 +2,53 @@ import classes.Day
 
 object Day01 : Day() {
     override val testData: List<String>
-        get() = TODO("Not yet implemented")
+        get() = """
+            L68
+            L30
+            R48
+            L5
+            R60
+            L55
+            L1
+            L99
+            R14
+            L82
+        """.trimIndent().split('\n')
+    override val part1ExampleSolution = 3
+    override val part2ExampleSolution = 6
 
-    override fun algorithmPartOne(input: List<String>): Any {
-        TODO("Not yet implemented")
+    override fun algorithmPart1(input: List<String>): Any {
+        var dial = 50
+
+        return input.count {
+
+            dial = if (it.first() == 'L')
+                (dial - it.drop(1).toInt()) % 100
+            else
+                (dial + it.drop(1).toInt()) % 100
+            dial == 0
+        }
     }
 
-    override fun algorithmPartTwo(input: List<String>): Any {
-        TODO("Not yet implemented")
-    }
+    override fun algorithmPart2(input: List<String>): Int {
+        var dial = 50
+        var count = 0
 
+        for (rotation in input) {
+            val dir = rotation.first()
+            val steps = rotation.drop(1).toInt()
+
+            repeat(steps) {
+                dial = when (dir) {
+                    'L' -> if (dial == 0) 99 else dial - 1
+                    'R' -> if (dial == 99) 0 else dial + 1
+                    else -> dial
+                }
+                if (dial == 0) count++
+            }
+        }
+        return count
+    }
 }
 
 fun main() {
